a7fd9d3e919459ed9fb534387e02babd7d9b9069
Doug-miguel <doug_miguel@outlook.com>
2022-08-17 21:58:55 -0300

    query-faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,10 +16,20 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn({ user, account, profile }) {
-      const { email } = user;
+    async signIn({ user }: any) {
+    const { email } = user
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
         return true;
       } catch {
         return false;
